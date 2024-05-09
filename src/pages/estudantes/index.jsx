4ec2932a28323dbf9a8cf0f6fb361d2c06ce4b2f
Okusindja de Almeida <okusindja@gmail.com>
2024-05-09 01:02:21 +0100

    🐛 fix: iteration bug

--- src/pages/estudantes/index.jsx
+++ src/pages/estudantes/index.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { Header } from "../../components/header/header";
 import Layout from "../../components/layout";
 import { Tabela } from "./tabela";
 export const Estudantes = () => {
   return (
     <Layout>
-      <Header title={'Estudantes'}/>
-      <Tabela lastHeader={'Ano'}></Tabela>
+      <Header title={"Estudantes"} />
+      <Tabela lastHeader={"Ano"}></Tabela>
     </Layout>
   );
 };
--- src/pages/estudantes/tabela.jsx
+++ src/pages/estudantes/tabela.jsx
@@ -1,79 +1,14 @@
-import React from "react"; 
 import "./styles.css";
 import { useQuery } from "@apollo/client";
 import { GETALUNOS } from "../../graphql/alunos";
 
-
-const DATA = [
-  {
-    nome: "Antonio ",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Nao abandona",
-  },
-  {
-    nome: "Elber",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Abandona",
-  },
-  {
-    nome: "Luis",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Nao abandona",
-  },
-  {
-    nome: "Andre",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "abandona",
-  },
-  {
-    nome: "Gomes",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Abandona",
-  },
-  {
-    nome: "Cleide",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Abandona",
-  },
-  {
-    nome: "Ana",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Nao abandona",
-  },
-  {
-    nome: "Loyde",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Nao abandona",
-  },
-  {
-    nome: "Filomena",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Abandona",
-  },
-  {
-    nome: "Felizarda",
-    matricula: "03953224",
-    curso: "Engenharia Informatica",
-    ano: "Abandona",
-  },
-];
-
 export const Tabela = (props) => {
-   const {data, loading, error} = useQuery(GETALUNOS);
-
-  const alunos = data;
-  console.log(alunos)
+  const { data, loading, error } = useQuery(GETALUNOS);
   if (loading) return <p>loading</p>;
   if (error) return <p>Ocorreu um erro</p>;
+
+  const alunos = data.alunos;
+  console.log(alunos);
   return (
     <div className="table-container">
       <table className="table-alunos">
@@ -81,13 +16,14 @@ export const Tabela = (props) => {
           <th>Nome</th>
           <th>Matricula</th>
           <th>Curso</th>
+          {/* eslint-disable-next-line react/prop-types */}
           <th>{props.lastHeader}</th>
         </tr>
         {alunos.map((item, index) => (
           <tr style={{ color: "black" }} key={index}>
             <td style={{ color: "black" }}>{item.name}</td>
             <td style={{ color: "black" }}>{item.matricula}</td>
-            <td style={{ color: "black" }}>{item.curso.nome}</td>
+            <td style={{ color: "black" }}>{item.curso && item.curso.nome}</td>
             <td style={{ color: "black" }}>{item.anoAcademico}</td>
           </tr>
         ))}
